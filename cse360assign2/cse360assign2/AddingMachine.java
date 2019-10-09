68ba9114f1b35f8a5c3c30f61b8b0ec34fb3d7d7
Owner <Owner@DESKTOP-M0IESEJ.3521.dhcp.asu.edu>
2019-10-09 16:57:28 -0700

    Initial checkin of adding machine tool.

---
+++ cse360assign2/cse360assign2/AddingMachine.java
@@ -0,0 +1,40 @@
+/*
+ * Name: Sai Sandilya Babbepalli Venkata
+ * Id: 1212688228
+ * We are to make an adding machine that performs actions
+ * like adding, subtracting, and converting to a string. 
+ * 
+ */
+
+package cse360assign2;
+
+
+
+		public class AddingMachine {
+			
+
+			private int total;
+			public AddingMachine  () {
+				total = 0;  // not needed - included for clarity
+				
+			}
+			public int getTotal () {
+				return 0;
+				}
+			public void add (int value) {
+				
+			}
+			
+			public void subtract (int value) {
+				
+			}
+			
+			public String toString () {
+			 	return "";
+			}
+		
+			public void clear() {
+				
+			}
+		}
+	
